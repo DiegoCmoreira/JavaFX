c01e5b077142678600d3db0b3ab22a998cd2bf83
DiegoCmoreira <diego.moreira@sptech.school>
2023-05-03 10:17:41 -0300

    Novos exemplos de uso

---
+++ javafx4/src/gui/ViewController.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.util.Locale;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+	
+	@FXML
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResultado;
+	
+	@FXML
+	private Button btSoma;
+	
+	@FXML
+	public void onBtSomaAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double nu1 = Double.parseDouble(txtNumber1.getText());
+			double nu2 = Double.parseDouble(txtNumber2.getText());
+			
+			double soma = nu1 + nu2;
+			
+			labelResultado.setText(String.format("%.2f", soma));
+		} catch (NumberFormatException e){
+			Alerts.showAlert("Error", "Parse Error", e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+}
